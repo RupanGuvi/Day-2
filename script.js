ae1cb66cb79c8db787cada744ffbace4d9f494fa
RupanGuvi <rupanguvi@gmail.com>
2024-08-02 20:26:37 +0530

    updated

---
+++ script.js
@@ -0,0 +1,51 @@
+console.log("Welcome to day 2");
+
+/* Var Example
+var a = 10;
+a=20; //reassign
+var a = 30; //redeclare
+console.log(a);
+*/
+
+/*let example
+let b =10;
+b=30; //reassign
+console.log(b);
+*/
+
+/*const example
+const c = 10;
+console.log(c);
+*/
+
+/*Window methods/events
+alert("Click Ok to enter into the website")
+ let result = prompt("Enter your Name")
+console.log(result)
+let result1= confirm("Are you sure to enter the webiste")
+console.log(result1);
+*/
+
+/*undefined example
+let a;
+console.log(a);
+*/
+
+/*null example
+let num = null;
+console.log(num);
+console.log(typeof(num));
+*/
+
+/*Object Example
+let person = {
+    firstname:"John",
+    lastname:"Doe",
+    age:35,
+    gender:"Male",
+    phonenumber:9876543210,
+    skills:["Javascript","HTML","CSS","Reactjs"]
+}
+console.log(person);
+*/
+
